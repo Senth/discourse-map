ca3a29d3792c0a1c2e68bf56aaa983e837260eb8
senth <senth.wallace@gmail.com>
2018-05-26 15:30:56 +0200

    Change config file from .ini to .py

--- discoursemap/__main__.py
+++ discoursemap/__main__.py
@@ -1,23 +1,13 @@
 import logging
-from configparser import ConfigParser
 
+from .config import Config
 from .api import Api
 
 logger = logging.getLogger(__name__)
 
 
 def main():
-    config = ConfigParser()
-    config.read('discoursemap/config.ini')
-
-    authentication = config['authentication']
-    url = authentication['url']
-    username = authentication['username']
-    api_key = authentication['api_key']
-
-    logger.debug('Url: ' + url + ', Username: ' + username + ', Key: ' + api_key)
-
-    api = Api(url, username, api_key)
+    api = Api(Config.SERVER_URL, Config.API_USERNAME, Config.API_KEY)
     users = api.get_user('Matteus')
 
 
---
+++ discoursemap/config.example.py
@@ -0,0 +1,4 @@
+class Config:
+    API_KEY = 'very-long-api-key'
+    API_USERNAME = 'username'
+    SERVER_URL = 'https://yourserver.com'
